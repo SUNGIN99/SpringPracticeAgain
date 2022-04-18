1fb2ac0abc9f923d68f0faefbe13825886b3ca97
SUNGIN99 <99_insung@naver.com>
2022-04-18 21:40:27 +0900

    22.04.18 post해서 값 자바에 얻어오는것까지.. 일단 함 머리개아픔 복습 할것

---
+++ SpringPracticeAgain/src/main/java/insung/SpringPracticeAgain/SpringPracticeAgainApplication.java
@@ -0,0 +1,13 @@
+package insung.SpringPracticeAgain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeAgainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeAgainApplication.class, args);
+	}
+
+}
---
+++ SpringPracticeAgain/src/test/java/insung/SpringPracticeAgain/SpringPracticeAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package insung.SpringPracticeAgain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeAgainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
